e4f6dc1cacaedbff43199cea2065a59f0fd083a4
Vladimir Trefilov <trevld26@gmail.com>
2021-10-24 21:20:29 +0400

    feat(Button): добавить возможность раширения компонента атрибутами

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import classNames from 'classnames'
 import './Button.scss'
 
-function Button (props) {
-  return <button className="btn">{props.children}</button>
+function Button ({ children, lg, primary, ...attrs }) {
+  const className = classNames('btn', {
+    btn_lg: lg,
+    btn_primary: primary
+  })
+
+  return <button className={className} {...attrs}>{children}</button>
 }
 
 Button.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  lg: PropTypes.bool,
+  primary: PropTypes.bool
+}
+
+Button.defaultProps = {
+  lg: false
 }
 
 export default Button
